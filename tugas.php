498b4db624dadd653b924d4542299eeb38ee7eb9
alamrkgit <alamrk.informatic@gmail.com>
2017-04-14 08:16:20 +0700

    first commit

---
+++ tugas.php
@@ -0,0 +1,280 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan Membuat Form</title>
+</head>
+<body>
+
+<!--Membuat Form-->
+<h1>Formulir Mahasiswa</h1>	
+<form>
+	<table>
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama" placeholder="Nama"></td>
+		</tr>
+		<tr>
+			<td>NPM</td>
+			<td>:</td>
+			<td><input type="text" name="npm" placeholder="NPM"></td>
+		</tr>
+		<tr>
+			<td>Kelas</td>
+			<td>:</td>
+			<td><input type="text" name="kelas" placeholder="Kelas"></td>
+		</tr>
+		<tr>
+			<td>Jenis Kelamin</td>
+			<td>:</td>
+			<td>
+			<input type="radio" name="j_kel" value="Male">Male
+			<input type="radio" name="j_kel" value="Female">Female
+			</td>
+		</tr>
+		<tr>
+			<td>Tanggal Lahir</td>
+			<td>:</td>
+			<td>
+				<input type="date" name="tgl_lahir">
+			</td>
+		</tr>
+		<tr>
+		<tr>
+			<td>Alamat</td>
+			<td>:</td>
+			<td>
+				<textarea name="alamat" placeholder="Alamat"></textarea>
+			</td>
+		</tr>
+		<tr>
+			<td>Hobi</td>
+			<td>:</td>
+			<td>
+				<input type="checkbox" name="hobi" value="Main Game">Main Game
+				<input type="checkbox" name="hobi" value="Belajar">Belajar
+				<input type="checkbox" name="hobi" value="Menonton Film">Menonton Film
+			</td>
+		</tr>
+			<td>Jurusan</td>
+			<td>:</td>
+			<td>
+				<select name="jurusan">
+					<option>--Pilih--
+					<option>Sistem Informasi
+					<option>Teknik Informatika
+					<option>Teknik Inustri
+					<option>Psikologi
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td>Fakultas</td>
+			<td>:</td>
+			<td>
+				<select name="fakultas">
+					<option>--Pilih--
+					<option>Teknologi Industri
+					<option>Ilmu Komputer
+					<option>Psikologi
+					<option>Ekonomi
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<input type="submit" value="Submit">
+				<input type="reset" value="Reset">
+			</td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan Membuat Form</title>
+</head>
+<body>
+ 
+<!--Membuat Form-->
+<h1>Formulir Mahasiswa</h1>	
+<form>
+	<table>
+		<tr>
+			<td>Nama</td>
+			<td>:</td>
+			<td><input type="text" name="nama" placeholder="Nama"></td>
+		</tr>
+		<tr>
+			<td>NPM</td>
+			<td>:</td>
+			<td><input type="text" name="npm" placeholder="NPM"></td>
+		</tr>
+		<tr>
+			<td>Kelas</td>
+			<td>:</td>
+			<td><input type="text" name="kelas" placeholder="Kelas"></td>
+		</tr>
+		<tr>
+			<td>Jenis Kelamin</td>
+			<td>:</td>
+			<td>
+			<input type="radio" name="j_kel" value="Male">Male
+			<input type="radio" name="j_kel" value="Female">Female
+			</td>
+		</tr>
+		<tr>
+			<td>Tanggal Lahir</td>
+			<td>:</td>
+			<td>
+				<input type="date" name="tgl_lahir">
+			</td>
+		</tr>
+		<tr>
+		<tr>
+			<td>Alamat</td>
+			<td>:</td>
+			<td>
+				<textarea name="alamat" placeholder="Alamat"></textarea>
+			</td>
+		</tr>
+		<tr>
+			<td>Hobi</td>
+			<td>:</td>
+			<td>
+				<input type="checkbox" name="hobi" value="Main Game">Main Game
+				<input type="checkbox" name="hobi" value="Belajar">Belajar
+				<input type="checkbox" name="hobi" value="Menonton Film">Menonton Film
+			</td>
+		</tr>
+			<td>Jurusan</td>
+			<td>:</td>
+			<td>
+				<select name="jurusan">
+					<option>--Pilih--
+					<option>Sistem Informasi
+					<option>Teknik Informatika
+					<option>Teknik Inustri
+					<option>Psikologi
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td>Fakultas</td>
+			<td>:</td>
+			<td>
+				<select name="fakultas">
+					<option>--Pilih--
+					<option>Teknologi Industri
+					<option>Ilmu Komputer
+					<option>Psikologi
+					<option>Ekonomi
+				</select>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<input type="submit" value="Submit">
+				<input type="reset" value="Reset">
+			</td>
+		</tr>
+	</table>
+</form>
+</body>
+</html>
